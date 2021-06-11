c111103566a2cb6154e185ee3bbbd408f686ed07
Rick Brown <ricky.brown.00@gmail.com>
2021-06-11 15:34:02 +0100

    Initial Commit

---
+++ swiftui-mvvm-todo-list/swiftui_mvvm_todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_mvvm_todo_listApp.swift
+//  swiftui-mvvm-todo-list
+//
+//  Created by Rick Brown on 11/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_mvvm_todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
