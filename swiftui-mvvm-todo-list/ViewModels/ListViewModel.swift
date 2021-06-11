bbf31816171983404b9053ee37a557cc1377ab49
Rick Brown <ricky.brown.00@gmail.com>
2021-06-11 23:10:38 +0100

    Adds persistence layer via basic UserDefaults (hacky tbh)

--- swiftui-mvvm-todo-list/ViewModels/ListViewModel.swift
+++ swiftui-mvvm-todo-list/ViewModels/ListViewModel.swift
@@ -8,20 +8,19 @@
 import Foundation
 
 class ListViewModel: ObservableObject {
-  @Published var todoItems: Array<TodoItem> = []
-  
-  init() {
-    getTodoItems()
+  @Published var todoItems: Array<TodoItem> = [] {
+    /// Everytime the array is mutated, save the new array into `UserDefaults`
+    didSet {
+      saveCurrentTodoItems()
+    }
   }
   
-  func getTodoItems() -> Void {
-    let newTodoItems = [
-      TodoItem(title: "1st todo item", isCompleted: false),
-      TodoItem(title: "2nd todo item", isCompleted: true),
-      TodoItem(title: "3rd todo item", isCompleted: false)
-    ]
-    
-    todoItems.append(contentsOf: newTodoItems)
+  /// A `ViewModel` scoped `String` to use as a `forKey` with `UserDefaults`
+  let itemsKey: String = "todo_items_list"
+  
+  /// Initialize the `todoItems` array
+  init() {
+    getCurrentTodoItems()
   }
   
   /// CRUD OPERATIONS
@@ -32,6 +31,7 @@ class ListViewModel: ObservableObject {
     todoItems.append(newTodoItem)
   }
   /// Remove
+  /// The `perform` parameter in the `ListView` `ForEach.onDelete {}` modifier will supply the `IndexSet`
   func deleteTodoItem(indexSet: IndexSet) -> Void {
     todoItems.remove(atOffsets: indexSet)
   }
@@ -42,12 +42,31 @@ class ListViewModel: ObservableObject {
     }
   }
   /// Delete
+  /// The `perform` parameter in the `ListView` `ForEach.onMove {}` modifier will supply the `IndexSet`
   func moveTodoItem(from startOffset: IndexSet, to finishOffset: Int) -> Void {
     todoItems.move(fromOffsets: startOffset, toOffset: finishOffset)
   }
   
+  /// Persistance Layer: `UserDefaults` - requires using the `Codable` protocol on the `TodoItem` model.
+  func saveCurrentTodoItems() -> Void {
+    /// `Codable` will convert the `todoItems` array into `JSON` format, the `JSON` in a `Data` type.
+    /// The coded `Data` type can then be passed to `UserDefaults.standard.set(: Any?, forKey: String)`,
+    /// The `forKey` parameter is a `constant`variable. defined in the parent scope, as a `String`.
+    /// This `String` is then used to identify the data stored in `UserDefaults`.
+    if let encodedData = try? JSONEncoder().encode(todoItems) {
+      UserDefaults.standard.set(encodedData, forKey: itemsKey)
+    }
+  }
   
-  
+  func getCurrentTodoItems() -> Void {
+    /// `Codable` will convert the 'todoItems JSON Blob' (`Data` type) back into `JSON` format
+    guard
+      let data = UserDefaults.standard.data(forKey: itemsKey),
+      let existingTodoItems = try? JSONDecoder().decode(Array<TodoItem>.self, from: data)
+      else { return }
+    
+    self.todoItems = existingTodoItems
+  }
   
 }
 
